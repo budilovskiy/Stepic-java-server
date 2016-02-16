42dff11a88748e942361b3b8ee8018c2dc44df10
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2016-02-16 22:04:42 +0200

    new commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import servlets.Frontend;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(frontend), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
+        server.join();
+    }
+}
