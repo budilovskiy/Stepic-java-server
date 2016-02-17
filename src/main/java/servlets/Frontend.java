6661a619ea2a54013063d09d070322d0048e75d5
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2016-02-17 19:08:18 +0200

    working

--- src/main/java/servlets/Frontend.java
+++ src/main/java/servlets/Frontend.java
@@ -14,11 +14,6 @@ public class Frontend extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-        // TODO: отправить не html страницу c ответом, а просто текст.
-        // Все, что вам нужно - это использовать response.getWriter.println(ваша строка).
-        // Ну и все остальное, что нужно в doGet.
-        // Тогда у вас нормально отобразится строчка, которую подставили в key и вы все пройдете.
-        
         String value = request.getParameter("key");
         response.getWriter().println(value == null ? "" : value);
         response.setContentType("text/html;charset=utf-8");
