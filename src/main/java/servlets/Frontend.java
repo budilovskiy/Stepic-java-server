4d2ccc4e1b095a1aeab066143cc0e0a19b290dac
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2016-02-17 17:16:16 +0200

    Update Frontend.java

--- src/main/java/servlets/Frontend.java
+++ src/main/java/servlets/Frontend.java
@@ -1,14 +1,10 @@
 package servlets;
 
-import templater.PageGenerator;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by Maxim on 15.02.2016.
